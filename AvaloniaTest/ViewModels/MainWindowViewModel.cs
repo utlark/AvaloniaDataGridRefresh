2cb3c59240918d556d3f019e70bb20de43406fea
Yuriy Zubkov <utlark@ya.ru>
2023-05-31 20:50:53 +0500

    Написал несколько режимов работы и протестировал их

--- AvaloniaTest/ViewModels/MainWindowViewModel.cs
+++ AvaloniaTest/ViewModels/MainWindowViewModel.cs
@@ -1,43 +1,108 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Reactive.Linq;
+using System.Timers;
+using Avalonia.Threading;
 using ReactiveUI;
 
 namespace AvaloniaTest.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private List<Person> _people = new();
+    private readonly Random _random = new();
 
-    public List<Person> People
+    private readonly Stack<Person> _stack = new();
+    private ObservableCollection<Person> _people = new();
+
+    public MainWindowViewModel()
+    {
+        var mode = RunMode.CorrectObservableCollection;
+
+        Observable.Interval(TimeSpan.FromSeconds(1))
+            .ObserveOn(AvaloniaScheduler.Instance)
+            .Subscribe(_ =>
+            {
+                switch (mode)
+                {
+                    case RunMode.Observable:
+                        People.Add(new Person
+                        {
+                            FirstName = _random.Next(1000).ToString(),
+                            LastName = _random.Next(1000).ToString()
+                        });
+                        break;
+                    case RunMode.TimerTest:
+                    {
+                        if (_stack.TryPop(out var newPerson))
+                            People.Add(newPerson);
+                        break;
+                    }
+                }
+            });
+
+        var timer = new Timer();
+        timer.Interval = 500;
+        timer.AutoReset = true;
+        timer.Elapsed += (_, _) =>
+        {
+            switch (mode)
+            {
+                case RunMode.TimerTest:
+                    _stack.Push(new Person
+                    {
+                        FirstName = _random.Next(1000).ToString(),
+                        LastName = _random.Next(1000).ToString()
+                    });
+                    break;
+                case RunMode.VanillaObservableCollection:
+                    People.Add(new Person
+                    {
+                        FirstName = _random.Next(1000).ToString(),
+                        LastName = _random.Next(1000).ToString()
+                    });
+                    break;
+                case RunMode.CorrectObservableCollection:
+                    Dispatcher.UIThread.InvokeAsync(() =>
+                    {
+                        People.Add(new Person
+                        {
+                            FirstName = _random.Next(1000).ToString(),
+                            LastName = _random.Next(1000).ToString()
+                        });
+                    }, DispatcherPriority.Background);
+                    break;
+            }
+        };
+        timer.Start();
+    }
+
+    public ObservableCollection<Person> People
     {
         get => _people;
         set => this.RaiseAndSetIfChanged(ref _people, value);
     }
 
-    public MainWindowViewModel()
+    private enum RunMode
     {
-        People = new List<Person>
-        {
-            new()
-            {
-                FirstName = "1",
-                LastName = "2"
-            },
-            new()
-            {
-                FirstName = "3",
-                LastName = "4"
-            },
-            new()
-            {
-                FirstName = "5",
-                LastName = "6"
-            },
-        };
+        Observable = 0,
+        TimerTest = 1,
+        VanillaObservableCollection = 2,
+        CorrectObservableCollection = 3
     }
 
+    #region PersonClass
+
+#pragma warning disable CS8618
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
     public class Person
     {
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
     }
+#pragma warning restore CS8618
+
+    #endregion
 }
