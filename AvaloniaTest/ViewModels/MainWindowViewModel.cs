0dfcaf8c1d263258ddc7d84f547896c21001a056
Yuriy Zubkov <utlark@ya.ru>
2023-05-31 20:21:34 +0500

    Добавил DataGrid со статичными данными

--- AvaloniaTest/ViewModels/MainWindowViewModel.cs
+++ AvaloniaTest/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace AvaloniaTest.ViewModels;
+﻿using System.Collections.Generic;
+using ReactiveUI;
+
+namespace AvaloniaTest.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private List<Person> _people = new();
+
+    public List<Person> People
+    {
+        get => _people;
+        set => this.RaiseAndSetIfChanged(ref _people, value);
+    }
+
+    public MainWindowViewModel()
+    {
+        People = new List<Person>
+        {
+            new()
+            {
+                FirstName = "1",
+                LastName = "2"
+            },
+            new()
+            {
+                FirstName = "3",
+                LastName = "4"
+            },
+            new()
+            {
+                FirstName = "5",
+                LastName = "6"
+            },
+        };
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
 }
